4919575d16673dee5d3fa45cdebd1a45bda45018
Максим Козьянин <makc2214@gmail.com>
2021-06-07 13:00:39 +0300

    add gicha

---
+++ RestFullApiCorpitech/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace RestFullApiCorpitech
+{
+    [ApiController]
+    [Route("/user")]
+    public class UserController : Controller
+    {
+        BookContext db = new BookContext();
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add(55);
+            return new ObjectResult(list);
+        }
+
+    }
+}
