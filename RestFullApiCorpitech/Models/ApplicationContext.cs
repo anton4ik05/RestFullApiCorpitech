e20acb322ac8e39d13d3700dcaaf110a381722cf
Max <makc2214@gmail.com>
2021-06-08 13:28:14 +0300

    Auto stash before merge of "master" and "origin/master"

--- RestFullApiCorpitech/Models/ApplicationContext.cs
+++ RestFullApiCorpitech/Models/ApplicationContext.cs
@@ -25,9 +25,9 @@ namespace RestFullApiCorpitech.Models
             modelBuilder.Entity<User>().HasData(new User
             {
                 Id = new Guid("716C2E99-6F6C-4472-81A5-43C56E11637C"),
-                surname = "Иванов",
-                name = "Иван",
-                middlename = "Иванович",
+                Surname = "Иванов",
+                Name = "Иван",
+                Middlename = "Иванович"
                 dateOfEmployment = DateTime.Parse("11.11.2015" ),
                 dateOfStartVacation = DateTime.Parse("04.04.2016"),
                 dateOfEndVacation = DateTime.Parse("04.14.2016")
