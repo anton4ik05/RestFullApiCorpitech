fd5101a1f45dafa621830c181bacf0dc2be1a4c5
Max <makc2214@gmail.com>
2021-06-09 12:17:30 +0300

    Fix dateTime (again)

--- RestFullApiCorpitech/Models/User.cs
+++ RestFullApiCorpitech/Models/User.cs
@@ -44,13 +44,6 @@ namespace RestFullApiCorpitech.Models
         [DataType(DataType.Date)]
         public DateTime dateOfEndVacation { get; set; } = DateTime.MinValue;
 
-        [Display(Name = "Дата начала отпуска")]
-        public string dateOfStartVacation { get; set; }
-
-        [Display(Name = "Дата конца отпуска")]
-        public string dateOfEndVacation { get; set; }
->>>>>>>>> Temporary merge branch 2
-
         public Double value { get; set; } = 0;
 
     public Double eval(DateTime endDate)
