fb51167c38d324a29e59d070b634899ffeb80773
Max <makc2214@gmail.com>
2021-06-09 12:13:28 +0300

    Fix toha

--- RestFullApiCorpitech/Models/ApplicationContext.cs
+++ RestFullApiCorpitech/Models/ApplicationContext.cs
@@ -23,10 +23,7 @@ namespace RestFullApiCorpitech.Models
                 Id = new Guid("716C2E99-6F6C-4472-81A5-43C56E11637C"),
                 Surname = "Иванов",
                 Name = "Иван",
-                Middlename = "Иванович",
-                dateOfEmployment = Convert.ToString(DateTimeOffset.Parse("11/06/2014").ToUnixTimeSeconds()),
-                dateOfStartVacation = Convert.ToString(DateTimeOffset.Parse("05/01/2016").ToUnixTimeSeconds()),
-                dateOfEndVacation = Convert.ToString(DateTimeOffset.Parse("05/11/2016").ToUnixTimeSeconds())
+                Middlename = "Иванович"
             });
         }
 
