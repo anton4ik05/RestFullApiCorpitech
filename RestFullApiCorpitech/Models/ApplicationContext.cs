b5167bf55ebb96d82391f286c1c05cbc1d3207ac
Anton <aanto236375@gmail.com>
2021-06-10 12:33:39 +0300

    Update db

--- RestFullApiCorpitech/Models/ApplicationContext.cs
+++ RestFullApiCorpitech/Models/ApplicationContext.cs
@@ -18,16 +18,81 @@ namespace RestFullApiCorpitech.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(
+                new User[]
+                {
+                    new User 
+                    { 
+                        Id = Guid.NewGuid(),
+                        Surname = "Иванов",
+                        Name = "Иван",
+                        Middlename = "Иванович",
+                        dateOfEmployment = new DateTime(2015, 11, 11)
 
-            modelBuilder.Entity<User>().HasData(new User
-            {
-                Id = new Guid("716C2E99-6F6C-4472-81A5-43C56E11637C"),
-                Surname = "Иванов",
-                Name = "Иван",
-                Middlename = "Иванович",
-                dateOfEmployment = new DateTime(2015, 11, 11)
-            }
-            );
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Surname = "Пупкин",
+                        Name = "Дмитрий",
+                        Middlename = "Василиевич",
+                        dateOfEmployment = new DateTime(2020, 05, 05)
+
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Surname = "Козий",
+                        Name = "Максим",
+                        Middlename = "Яновичк",
+                        dateOfEmployment = new DateTime(2017, 09, 20)
+
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Surname = "Гордынский",
+                        Name = "Ян",
+                        Middlename = "Безотчествович",
+                        dateOfEmployment = new DateTime(2016, 12, 11)
+
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Surname = "Новиков",
+                        Name = "Антон",
+                        Middlename = "Сергеевич",
+                        dateOfEmployment = new DateTime(2021, 03, 25)
+
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Surname = "Васин",
+                        Name = "Василий",
+                        Middlename = "Васильевич",
+                        dateOfEmployment = new DateTime(2018, 02, 11)
+
+                    }, new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Surname = "Обжигайлов",
+                        Name = "Иван",
+                        Middlename = "Данилович",
+                        dateOfEmployment = new DateTime(2017, 07, 20)
+
+                    }, new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Surname = "Кузина",
+                        Name = "Альбина",
+                        Middlename = "Андреевна",
+                        dateOfEmployment = new DateTime(2019, 01, 01)
+
+                    },
+
+                });
 
             modelBuilder.Entity<User>().HasMany(c => c.Vacations).WithOne(e => e.user);
         }
