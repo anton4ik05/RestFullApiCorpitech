a016239ee9b5cf56ec1f744ab6c918c2abdcfab3
Max <makc2214@gmail.com>
2021-06-22 11:22:27 +0300

    Add AuthRequest, Response

---
+++ RestFullApiCorpitech/Models/DAO/AuthRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestFullApiCorpitech.Models.DAO
+{
+    public class AuthRequest
+    {
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Логин")]
+        private string login { get; set; }
+
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        private string password { get; set; }
+
+    }
+}
